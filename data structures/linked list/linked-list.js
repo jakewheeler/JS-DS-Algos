3c78d197089f6e676c9ff6d8b510d8f45f06f1cd
Jake Wheeler <jakewheeler17@gmail.com>
2020-03-25 20:11:36 -0400

    added tail pointer to linked list, added insertAfter method

--- data structures/linked list/linked-list.js
+++ data structures/linked list/linked-list.js
@@ -6,8 +6,9 @@ class MyNode {
 }
 
 class LinkedList {
-  constructor() {
-    this.head = null;
+  constructor(value) {
+    this.head = new MyNode(value);
+    this.tail = this.head.next;
   }
 
   // append data to the linked list
@@ -21,17 +22,38 @@ class LinkedList {
       current = current.next;
     }
     current.next = new MyNode(data);
+    this.tail = current.next;
   }
 
   // prepend data to the linked list
   prepend(data) {
     if (this.head === null) {
       this.head = new MyNode(data);
+      this.tail = this.head.next;
       return;
     }
     let newHead = new MyNode(data);
     newHead.next = this.head;
     this.head = newHead;
+  }
+
+  insertAfter(index, data) {
+    let currentLoc = 0;
+    let current = this.head;
+
+    while (current.next !== null) {
+      if (currentLoc === index) {
+        let newNode = new MyNode(data);
+        let oldNext = current.next;
+
+        // re-route pointers
+        current.next = newNode;
+        newNode.next = oldNext;
+        return;
+      }
+      currentLoc++;
+      current = current.next;
+    }
   }
 
   // delete first node with specified value
@@ -45,7 +67,11 @@ class LinkedList {
     let current = this.head;
 
     while (current.next !== null) {
-      if (current.next.data === data) {
+      if (current.next.data === data && current.next.data === this.tail.data) {
+        this.tail = current;
+        this.tail.next = null;
+        return;
+      } else if (current.next.data === data) {
         current.next = current.next.next;
         return;
       }
@@ -62,14 +88,19 @@ class LinkedList {
       current = current.next;
     }
     console.log(print);
+    console.log('head:', this.head.data);
+    console.log('tail:', this.tail.data);
   }
 }
 
-let ll = new LinkedList();
+let ll = new LinkedList(6);
 ll.append(3);
 ll.append(5);
 ll.prepend(1);
 ll.prepend(2);
 ll.prepend(5);
 ll.delete(1);
+ll.append(19);
+ll.insertAfter(0, 17);
+ll.delete(19);
 ll.print();
