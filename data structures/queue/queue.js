47ddcc61527070813218f605e3aee7f959093834
Jake Wheeler <jakewheeler17@gmail.com>
2020-04-15 19:02:28 -0400

    updated queue

--- data structures/queue/queue.js
+++ data structures/queue/queue.js
@@ -1,34 +1,73 @@
-class Queue {
-  constructor() {
-    this.q = new LinkedList(2);
-  }
-
-  enqueue(data) {
-    this.q.append(data);
-  }
-
-  dequeue() {
-    this.q.
-  }
-
-  isEmpty() {
-    return this.q.head === null;
-  }
-
-  peek() {
-    return this.q.head.data;
-  }
-
-  print() {
-    this.q.print();
+class MyNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek = () => this.first;
+
+  enqueue = (val) => {
+    let node = new MyNode(val);
+
+    // check if first is null
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+
+    this.length++;
+    return this.last;
+  };
+
+  dequeue = () => {
+    if (!this.first) return null;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    let oldFirst = this.first;
+    this.first = oldFirst.next;
+    this.length--;
+    return oldFirst;
+  };
+
+  isEmpty = () => this.length <= 0;
+
+  print = () => {
+    console.log('first:', this.first.value);
+    console.log('last:', this.last.value);
+    console.log('length:', this.length);
+    console.log('empty:', this.isEmpty());
+  };
+}
+
 let q = new Queue();
-q.enqueue(3);
-q.enqueue(2);
+
+// add to Q
 q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
+
+// remove the first element
 q.dequeue();
-q.enqueue(16);
-console.log(q.peek());
+
+// print results
 q.print();
+
+console.log('peek:', q.peek().value);
+// q.enqueue(1);
+// q.dequeue();
+// q.enqueue(16);
+// console.log(q.peek());
