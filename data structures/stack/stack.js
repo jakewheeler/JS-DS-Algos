dc52137a4e8d1543a2d66e51e57c220d9dacbd04
Jake Wheeler <jakewheeler17@gmail.com>
2020-04-14 20:58:28 -0400

    modified linked list code, added initial queue, updated stack to use linked list instead of array

--- data structures/stack/stack.js
+++ data structures/stack/stack.js
@@ -1,49 +1,98 @@
-// stack class built on top of an array
+// stack class built on top of a linked list
 
-class MyStack {
-  constructor() {
-    this.data = [];
-  }
-
-  push(elem) {
-    return this.data.unshift(elem);
-  }
-
-  pop() {
-    return this.data.shift();
-  }
-
-  peek() {
-    if (this.isEmpty()) return null;
-    return this.data[0];
-  }
-
-  print() {
-    console.log('top: ', this.data, ' :buttom');
-  }
-
-  isEmpty() {
-    return this.data.length === 0;
+class MyNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-let stack = new MyStack();
+class LinkedListStack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek = () => {
+    return this.top;
+  };
+
+  push = (val) => {
+    let node = new MyNode(val);
+    if (!this.bottom) {
+      this.bottom = node;
+      this.top = node;
+    } else {
+      let currentTop = this.top;
+      this.top = node;
+      this.top.next = currentTop;
+    }
+
+    this.length++;
+  };
+
+  pop = () => {
+    if (!this.top) return null;
+
+    let prevTop = this.top;
+
+    if (this.top === this.bottom) {
+      this.top = null;
+      this.bottom = null;
+      return prevTop;
+    }
+
+    this.top = this.top.next;
+
+    this.length--;
+    return prevTop;
+  };
+
+  isEmpty = () => this.length <= 0;
+
+  print = () => {
+    let current = this.top;
+    console.log('| top |');
+    while (current) {
+      console.log(`  [${current.value}]`);
+      current = current.next;
+    }
+    console.log('| bottom |');
+    console.log();
+
+    console.log('length:', this.length);
+
+    if (!this.top) return;
+
+    console.log('top:', this.top.value);
+    console.log('bottom:', this.bottom.value);
+  };
+}
+
+let llstack = new LinkedListStack();
 
 // add to top of stack
-stack.push(3);
-stack.push(1);
-stack.push(2);
+llstack.push(3);
+llstack.push(1);
+llstack.push(2);
+llstack.push(22);
+llstack.push(39);
+llstack.push(89);
 
-// remove from stack
-stack.pop();
-stack.pop();
-stack.pop();
-stack.pop();
-console.log(stack.isEmpty());
+// remove 89
+let poppedValue = llstack.pop();
+console.log('popped value:', poppedValue.value);
 
-// check peek
-stack.push(1);
-stack.push(33);
-console.log(stack.peek());
+// remove 39
+llstack.pop();
 
-stack.print();
+// add 12
+llstack.push(12);
+
+// print stack with 12 at top and 3 at bottom
+llstack.print();
+
+if (llstack.peek()) console.log('peek:', llstack.peek().value);
+
+console.log('isEmpty:', llstack.isEmpty());
