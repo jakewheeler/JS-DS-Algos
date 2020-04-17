c0fee07ec3e7f119a06d9330f752957db2007f49
Jake Wheeler <jakewheeler17@gmail.com>
2020-04-17 15:04:44 -0400

    added insert to bst

---
+++ data structures/trees/bst.js
@@ -0,0 +1,62 @@
+const traverse = (node) => {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+};
+
+class MyNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert = (value) => {
+    // create a new node
+    let newNode = new MyNode(value);
+
+    // if the root is empty, new node is the root
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      // otherwise, we traverse through the tree
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return newNode;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return newNode;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+
+    // remove
+  };
+
+  lookup = (value) => {};
+}
+
+const tree = new BST();
+tree.insert(9);
+tree.insert(18);
+tree.insert(20);
+tree.insert(20);
+tree.insert(170);
+tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)));
