b02b7028ab79bbf9a1ea00c4c80d10802fad5056
Jake Wheeler <jakewheeler17@gmail.com>
2020-04-19 22:25:15 -0400

    added remove to bst

--- data structures/trees/bst.js
+++ data structures/trees/bst.js
@@ -45,8 +45,6 @@ class BST {
         }
       }
     }
-
-    // remove
   };
 
   lookup = (value) => {
@@ -70,16 +68,98 @@ class BST {
       }
     }
   };
+
+  isLeafNode = (node) => {
+    if (!node.left && !node.right) return true;
+    return false;
+  };
+
+  remove = (value) => {
+    if (!this.root) {
+      return false;
+    }
+
+    let parentNode = null;
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        // go left
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        // go right
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        // no right child
+        if (!currentNode.right) {
+          // if parent node is null, we just make the root equal the left node
+          if (!parentNode) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+
+          // right child doesnt have a left child
+        } else if (currentNode.right.left === null) {
+          if (!parentNode) {
+            this.root = currentNode.left;
+          } else {
+            currentNode.right.left = currentNode.left;
+
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+
+          // right child has a left child
+        } else {
+          // find right child's left most child
+          let leftmost = currentNode.right.left;
+          let leftmostParent = currentNode.right;
+          while (!leftmost.left) {
+            leftmostParent = leftmost;
+            leftmost = leftmost.left;
+          }
+          // parent's left subtree is leftmost's right subtree
+          leftmostParent.left = leftmost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+
+          if (parentNode === null) {
+            this.root = leftmost;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost;
+            }
+          }
+        }
+        return true;
+      }
+    }
+  };
 }
 
 const tree = new BST();
-tree.insert(9);
-tree.insert(18);
-tree.insert(20);
-tree.insert(20);
-tree.insert(170);
-tree.insert(1);
+tree.insert(98);
+tree.insert(46);
+tree.insert(19);
+tree.insert(14);
+tree.insert(88);
+tree.insert(89);
+tree.insert(55);
+tree.insert(69);
+tree.insert(99);
+// tree.remove(89);
 console.log(JSON.stringify(traverse(tree.root)));
 
-let node = tree.lookup(9);
-console.log(node);
+// let node = tree.lookup(9);
+// console.log(node);
